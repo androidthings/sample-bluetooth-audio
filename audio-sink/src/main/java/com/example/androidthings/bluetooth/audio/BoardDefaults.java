4bed5369af29dccdf03acebe4e37b00600169e77
Dave Smith <smithdave@google.com>
2017-11-06 10:24:59 -0700

    Remove deprecated signals and diagrams.
    Bug: 67780299
    Bug: 64797207
    
    Change-Id: I30d2edbdb175a9e6d709229b0870925a6a982a43
    Signed-off-by: Dave Smith <smithdave@google.com>

--- audio-sink/src/main/java/com/example/androidthings/bluetooth/audio/BoardDefaults.java
+++ audio-sink/src/main/java/com/example/androidthings/bluetooth/audio/BoardDefaults.java
@@ -18,38 +18,21 @@ package com.example.androidthings.bluetooth.audio;
 
 import android.os.Build;
 
-import com.google.android.things.pio.PeripheralManagerService;
-
-import java.util.List;
-
 @SuppressWarnings("WeakerAccess")
 public class BoardDefaults {
-    private static final String DEVICE_EDISON_ARDUINO = "edison_arduino";
-    private static final String DEVICE_EDISON = "edison";
-    private static final String DEVICE_JOULE = "joule";
     private static final String DEVICE_RPI3 = "rpi3";
     private static final String DEVICE_IMX6UL_PICO = "imx6ul_pico";
-    private static final String DEVICE_IMX6UL_VVDN = "imx6ul_iopb";
     private static final String DEVICE_IMX7D_PICO = "imx7d_pico";
-    private static String sBoardVariant;
 
     /**
      * Return the GPIO pin with a button that will trigger the Pairing command.
      */
     public static String getGPIOForPairing() {
-        switch (getBoardVariant()) {
-            case DEVICE_EDISON_ARDUINO:
-                return "IO12";
-            case DEVICE_EDISON:
-                return "GP44";
-            case DEVICE_JOULE:
-                return "J7_71";
+        switch (Build.DEVICE) {
             case DEVICE_RPI3:
                 return "BCM21";
             case DEVICE_IMX6UL_PICO:
                 return "GPIO2_IO03";
-            case DEVICE_IMX6UL_VVDN:
-                return "GPIO3_IO01";
             case DEVICE_IMX7D_PICO:
                 return "GPIO_174";
             default:
@@ -61,43 +44,15 @@ public class BoardDefaults {
      * Return the GPIO pin with a button that will trigger the Disconnect All command.
      */
     public static String getGPIOForDisconnectAllBTDevices() {
-        switch (getBoardVariant()) {
-            case DEVICE_EDISON_ARDUINO:
-                return "IO13";
-            case DEVICE_EDISON:
-                return "GP45";
-            case DEVICE_JOULE:
-                return "J6_25";
+        switch (Build.DEVICE) {
             case DEVICE_RPI3:
                 return "BCM20";
             case DEVICE_IMX6UL_PICO:
                 return "GPIO4_IO22";
-            case DEVICE_IMX6UL_VVDN:
-                return "GPIO3_IO06";
             case DEVICE_IMX7D_PICO:
                 return "GPIO_175";
             default:
                 throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
         }
-    }
-
-    private static String getBoardVariant() {
-        if (sBoardVariant != null) {
-            return sBoardVariant;
-        }
-        sBoardVariant = Build.DEVICE;
-        // For the edison check the pin prefix
-        // to always return Edison Breakout pin name when applicable.
-        if (sBoardVariant.equals(DEVICE_EDISON)) {
-            PeripheralManagerService pioService = new PeripheralManagerService();
-            List<String> gpioList = pioService.getGpioList();
-            if (gpioList.size() != 0) {
-                String pin = gpioList.get(0);
-                if (pin.startsWith("IO")) {
-                    sBoardVariant = DEVICE_EDISON_ARDUINO;
-                }
-            }
-        }
-        return sBoardVariant;
     }
 }
