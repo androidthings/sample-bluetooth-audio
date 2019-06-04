053d9a66ac56f15fc9e05ea57822360ea6533d8e
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2019-06-04 17:33:26 +0200

    refactor: change package name and migrate to kotlin

---
+++ audio-sink/src/main/java/io/github/rosariopfernandes/btspeaksthings/A2dpSinkActivity.kt
@@ -0,0 +1,395 @@
+package io.github.rosariopfernandes.btspeaksthings
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothProfile
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.KeyEvent
+import com.example.androidthings.bluetooth.audio.BoardDefaults
+import com.google.android.things.bluetooth.BluetoothProfileManager
+import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.contrib.driver.button.ButtonInputDriver
+import java.io.IOException
+import java.util.*
+
+class A2dpSinkActivity : Activity() {
+
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mA2DPSinkProxy: BluetoothProfile? = null
+
+    private var mPairingButtonDriver: ButtonInputDriver? = null
+    private var mDisconnectAllButtonDriver: ButtonInputDriver? = null
+
+    private var mTtsEngine: TextToSpeech? = null
+
+    /**
+     * Handle an intent that is broadcast by the Bluetooth adapter whenever it changes its
+     * state (after calling enable(), for example).
+     * Action is [BluetoothAdapter.ACTION_STATE_CHANGED] and extras describe the old
+     * and the new states. You can use this intent to indicate that the device is ready to go.
+     */
+    private val mAdapterStateChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val oldState = intent.getPreviousAdapterState()
+            val newState = intent.getCurrentAdapterState()
+            Log.d(TAG, "Bluetooth Adapter changing state from $oldState to $newState")
+            if (newState == BluetoothAdapter.STATE_ON) {
+                Log.i(TAG, "Bluetooth Adapter is ready")
+                initA2DPSink()
+            }
+        }
+    }
+
+    /**
+     * Handle an intent that is broadcast by the Bluetooth A2DP sink profile whenever a device
+     * connects or disconnects to it.
+     * Action is [A2dpSinkHelper.ACTION_CONNECTION_STATE_CHANGED] and
+     * extras describe the old and the new connection states. You can use it to indicate that
+     * there's a device connected.
+     */
+    private val mSinkProfileStateChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == ACTION_CONNECTION_STATE_CHANGED) {
+                val oldState = intent.getPreviousProfileState()
+                val newState = intent.getCurrentProfileState()
+                val device = intent.getDevice()
+                Log.d(TAG, "Bluetooth A2DP sink changing connection state from $oldState" +
+                        " to $newState device $device")
+                device?.let {
+                    val deviceName = Objects.toString(device.name, "a device")
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        speak("Connected to $deviceName")
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        speak("Disconnected from $deviceName")
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Handle an intent that is broadcast by the Bluetooth A2DP sink profile whenever a device
+     * starts or stops playing through the A2DP sink.
+     * Action is [A2dpSinkHelper.ACTION_PLAYING_STATE_CHANGED] and
+     * extras describe the old and the new playback states. You can use it to indicate that
+     * there's something playing. You don't need to handle the stream playback by yourself.
+     */
+    private val mSinkProfilePlaybackChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == ACTION_PLAYING_STATE_CHANGED) {
+                val oldState = intent.getPreviousProfileState()
+                val newState = intent.getCurrentProfileState()
+                val device = intent.getDevice()
+                Log.d(TAG, "Bluetooth A2DP sink changing playback state from $oldState" +
+                        " to $newState device $device" )
+                if (device != null) {
+                    if (newState == STATE_PLAYING) {
+                        Log.i(TAG, "Playing audio from device ${device.address}")
+                    } else if (newState == STATE_NOT_PLAYING) {
+                        Log.i(TAG, "Stopped playing audio from ${device.address}")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter == null) {
+            Log.w(TAG, "No default Bluetooth adapter. Device likely does not support bluetooth.")
+            return
+        }
+
+        // We use Text-to-Speech to indicate status change to the user
+        initTts()
+
+        registerReceiver(mAdapterStateChangeReceiver, IntentFilter(
+                BluetoothAdapter.ACTION_STATE_CHANGED))
+        registerReceiver(mSinkProfileStateChangeReceiver, IntentFilter(
+                ACTION_CONNECTION_STATE_CHANGED))
+        registerReceiver(mSinkProfilePlaybackChangeReceiver, IntentFilter(
+                ACTION_PLAYING_STATE_CHANGED))
+
+        mBluetoothAdapter?.let {
+            if (it.isEnabled) {
+                Log.d(TAG, "Bluetooth Adapter is already enabled.")
+                initA2DPSink()
+            } else {
+                Log.d(TAG, "Bluetooth adapter not enabled. Enabling.")
+                it.enable()
+            }
+        }
+
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        when (keyCode) {
+            KeyEvent.KEYCODE_P -> {
+                // Enable Pairing mode (discoverable)
+                enableDiscoverable()
+                return true
+            }
+            KeyEvent.KEYCODE_D -> {
+                // Disconnect any currently connected devices
+                disconnectConnectedDevices()
+                return true
+            }
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+
+        try {
+            mPairingButtonDriver?.let {
+                it.close()
+            }
+        } catch (e: IOException) { /* close quietly */
+        }
+
+        try {
+            mDisconnectAllButtonDriver?.let {
+                it.close()
+            }
+        } catch (e: IOException) { /* close quietly */
+        }
+
+        unregisterReceiver(mAdapterStateChangeReceiver)
+        unregisterReceiver(mSinkProfileStateChangeReceiver)
+        unregisterReceiver(mSinkProfilePlaybackChangeReceiver)
+
+        mA2DPSinkProxy?.let {
+            mBluetoothAdapter!!.closeProfileProxy(A2DP_SINK_PROFILE, mA2DPSinkProxy)
+        }
+
+        mTtsEngine?.let {
+            it.stop()
+            it.shutdown()
+        }
+
+        // we intentionally leave the Bluetooth adapter enabled, so that other samples can use it
+        // without having to initialize it.
+    }
+
+    private fun setupBTProfiles() {
+        val bluetoothProfileManager = BluetoothProfileManager.getInstance()
+        val enabledProfiles = bluetoothProfileManager.enabledProfiles
+        if (!enabledProfiles.contains(A2DP_SINK_PROFILE)) {
+            Log.d(TAG, "Enabling A2dp sink mode.")
+            val toDisable = listOf(BluetoothProfile.A2DP)
+            val toEnable = listOf(A2DP_SINK_PROFILE, AVRCP_CONTROLLER_PROFILE)
+            bluetoothProfileManager.enableAndDisableProfiles(toEnable, toDisable)
+        } else {
+            Log.d(TAG, "A2dp sink profile is enabled.")
+        }
+    }
+
+    /**
+     * Initiate the A2DP sink.
+     */
+    private fun initA2DPSink() {
+        if (mBluetoothAdapter == null || !mBluetoothAdapter!!.isEnabled) {
+            Log.e(TAG, "Bluetooth adapter not available or not enabled.")
+            return
+        }
+        setupBTProfiles()
+        Log.d(TAG, "Set up Bluetooth Adapter name and profile")
+        mBluetoothAdapter!!.name = ADAPTER_FRIENDLY_NAME
+        mBluetoothAdapter!!.getProfileProxy(this, object : BluetoothProfile.ServiceListener {
+            override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+                mA2DPSinkProxy = proxy
+                enableDiscoverable()
+            }
+
+            override fun onServiceDisconnected(profile: Int) {
+
+            }
+        }, A2DP_SINK_PROFILE)
+
+        configureButton()
+    }
+
+    /**
+     * Enable the current [BluetoothAdapter] to be discovered (available for pairing) for
+     * the next [.DISCOVERABLE_TIMEOUT_MS] ms.
+     */
+    private fun enableDiscoverable() {
+        Log.d(TAG, "Registering for discovery.")
+        val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_TIMEOUT_MS)
+        }
+        startActivityForResult(discoverableIntent, REQUEST_CODE_ENABLE_DISCOVERABLE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_ENABLE_DISCOVERABLE) {
+            Log.d(TAG, "Enable discoverable returned with result $resultCode")
+
+            // ResultCode, as described in BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE, is either
+            // RESULT_CANCELED or the number of milliseconds that the device will stay in
+            // discoverable mode. In a regular Android device, the user will see a popup requesting
+            // authorization, and if they cancel, RESULT_CANCELED is returned. In Android Things,
+            // on the other hand, the authorization for pairing is always given without user
+            // interference, so RESULT_CANCELED should never be returned.
+            if (resultCode == RESULT_CANCELED) {
+                Log.e(TAG, "Enable discoverable has been cancelled by the user. " +
+                        "This should never happen in an Android Things device.")
+                return
+            }
+            Log.i(TAG, "Bluetooth adapter successfully set to discoverable mode. " +
+                    "Any A2DP source can find it with the name $ADAPTER_FRIENDLY_NAME"  +
+                    " and pair for the next $DISCOVERABLE_TIMEOUT_MS ms. " +
+                    "Try looking for it on your phone, for example.")
+
+            // There is nothing else required here, since Android framework automatically handles
+            // A2DP Sink. Most relevant Bluetooth events, like connection/disconnection, will
+            // generate corresponding broadcast intents or profile proxy events that you can
+            // listen to and react appropriately.
+
+            speak("Bluetooth audio sink is discoverable for $DISCOVERABLE_TIMEOUT_MS" +
+                    " milliseconds. Look for a device named $ADAPTER_FRIENDLY_NAME")
+
+        }
+    }
+
+    private fun disconnectConnectedDevices() {
+        if (mA2DPSinkProxy == null || mBluetoothAdapter == null || !mBluetoothAdapter!!.isEnabled) {
+            return
+        }
+        speak("Disconnecting devices")
+        for (device in mA2DPSinkProxy!!.connectedDevices) {
+            Log.i(TAG, "Disconnecting device $device")
+            device.disconnect(mA2DPSinkProxy!!)
+        }
+    }
+
+    private fun configureButton() {
+        try {
+            mPairingButtonDriver = ButtonInputDriver(BoardDefaults.getGPIOForPairing(),
+                    Button.LogicState.PRESSED_WHEN_LOW, KeyEvent.KEYCODE_P)
+            mPairingButtonDriver!!.register()
+            mDisconnectAllButtonDriver = ButtonInputDriver(
+                    BoardDefaults.getGPIOForDisconnectAllBTDevices(),
+                    Button.LogicState.PRESSED_WHEN_LOW, KeyEvent.KEYCODE_D)
+            mDisconnectAllButtonDriver!!.register()
+        } catch (e: IOException) {
+            Log.w(TAG, "Could not register GPIO button drivers. Use keyboard events to trigger" +
+                    " the functions instead", e)
+        }
+
+    }
+
+    private fun initTts() {
+        mTtsEngine = TextToSpeech(this@A2dpSinkActivity,
+                TextToSpeech.OnInitListener { status ->
+                    if (status == TextToSpeech.SUCCESS) {
+                        mTtsEngine!!.language = Locale.US
+                    } else {
+                        Log.w(TAG, "Could not open TTS Engine (onInit status=$status" +
+                                "). Ignoring text to speech")
+                        mTtsEngine = null
+                    }
+                })
+    }
+
+
+    private fun speak(utterance: String) {
+        Log.i(TAG, utterance)
+        mTtsEngine?.let {
+            it.speak(utterance, TextToSpeech.QUEUE_ADD, null, UTTERANCE_ID)
+        }
+    }
+
+    companion object {
+        private const val TAG = "A2dpSinkActivity"
+
+        private const val ADAPTER_FRIENDLY_NAME = "Bluetooth Speaks Things"
+        private const val DISCOVERABLE_TIMEOUT_MS = 300
+        private const val REQUEST_CODE_ENABLE_DISCOVERABLE = 100
+        private const val UTTERANCE_ID = "io.github.rosariopfernandes.btspeaksthings.UTTERANCE_ID"
+
+        /**
+         * Profile number for A2DP_SINK profile.
+         */
+        private const val A2DP_SINK_PROFILE = 11
+
+        /**
+         * Profile number for AVRCP_CONTROLLER profile.
+         */
+        private const val AVRCP_CONTROLLER_PROFILE = 12
+
+        /**
+         * Intent used to broadcast the change in connection state of the A2DP Sink
+         * profile.
+         *
+         *
+         * This intent will have 3 extras:
+         *
+         *  *  [BluetoothProfile.EXTRA_STATE] - The current state of the profile.
+         *  *  [BluetoothProfile.EXTRA_PREVIOUS_STATE]- The previous state of the
+         * profile.
+         *  *  [BluetoothDevice.EXTRA_DEVICE] - The remote device.
+         *
+         *
+         *
+         * [BluetoothProfile.EXTRA_STATE] or [BluetoothProfile.EXTRA_PREVIOUS_STATE]
+         * can be any of [BluetoothProfile.STATE_DISCONNECTED],
+         * [BluetoothProfile.STATE_CONNECTING], [BluetoothProfile.STATE_CONNECTED],
+         * [BluetoothProfile.STATE_DISCONNECTING].
+         *
+         *
+         * Requires [android.Manifest.permission.BLUETOOTH] permission to
+         * receive.
+         */
+        private const val ACTION_CONNECTION_STATE_CHANGED = "android.bluetooth.a2dp-sink.profile.action.CONNECTION_STATE_CHANGED"
+
+        /**
+         * Intent used to broadcast the change in the Playing state of the A2DP Sink
+         * profile.
+         *
+         *
+         * This intent will have 3 extras:
+         *
+         *  *  [BluetoothProfile.EXTRA_STATE] - The current state of the profile.
+         *  *  [BluetoothProfile.EXTRA_PREVIOUS_STATE]- The previous state of the
+         * profile.
+         *  *  [BluetoothDevice.EXTRA_DEVICE] - The remote device.
+         *
+         *
+         *
+         * [BluetoothProfile.EXTRA_STATE] or [BluetoothProfile.EXTRA_PREVIOUS_STATE]
+         * can be any of [.STATE_PLAYING], [.STATE_NOT_PLAYING],
+         *
+         *
+         * Requires [android.Manifest.permission.BLUETOOTH] permission to
+         * receive.
+         */
+        private const val ACTION_PLAYING_STATE_CHANGED = "android.bluetooth.a2dp-sink.profile.action.PLAYING_STATE_CHANGED"
+
+        /**
+         * A2DP sink device is streaming music. This state can be one of
+         * [BluetoothProfile.EXTRA_STATE] or [BluetoothProfile.EXTRA_PREVIOUS_STATE] of
+         * [.ACTION_PLAYING_STATE_CHANGED] intent.
+         */
+        private const val STATE_PLAYING = 10
+
+        /**
+         * A2DP sink device is NOT streaming music. This state can be one of
+         * [BluetoothProfile.EXTRA_STATE] or [BluetoothProfile.EXTRA_PREVIOUS_STATE] of
+         * [.ACTION_PLAYING_STATE_CHANGED] intent.
+         */
+        private const val STATE_NOT_PLAYING = 11
+    }
+}
